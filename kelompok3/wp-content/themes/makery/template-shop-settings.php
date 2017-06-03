4c26a3c3f95abf7ac0bd225807a19cb09cf7ea7d
DESKTOP-GCEFE9L\DANI-PC <daniangga98@hotmail.com>
2017-06-03 15:49:55 +0700

    Penataan tata bahasa dan kode untuk aturan toko

--- kelompok3/wp-content/themes/makery/template-shop-settings.php
+++ kelompok3/wp-content/themes/makery/template-shop-settings.php
@@ -11,9 +11,9 @@ Template Name: Shop
 	</div>
 	<?php ThemexInterface::renderTemplateContent('shop-settings'); ?>
 	<?php if(ThemexCore::checkOption('shop_multiple')) { ?>
-	<span class="secondary"><?php _e('Toko ini tidak ada', 'makery'); ?></span>
+	<span class="secondary"><?php _e('Toko yang anda cari tidak ada', 'makery'); ?></span>
 	<?php } else if(ThemexShop::$data['status']=='pending') { ?>
-	<span class="secondary"><?php _e('Toko ini sedang di review', 'makery'); ?></span>
+	<span class="secondary"><?php _e('Toko ini sedang di nilai', 'makery'); ?></span>
 	<?php } else { ?>
 	<?php if(!empty(ThemexShop::$data['ID'])) { ?>
 	<form action="" method="POST" id="shop_form_<?php echo ThemexShop::$data['ID']; ?>" class="site-form element-autosave" data-default="shop_form">
@@ -70,25 +70,27 @@ Template Name: Shop
 			</tbody>
 		</table>
 		<div class="profile-editor">
-			<h5><?php _e('Deskripsi Toko', 'makery'); ?></h5>
+			<h5><?php _e('Deskripsi', 'makery'); ?></h5>
 			<?php ThemexInterface::renderEditor('content', ThemexShop::$data['profile']['content']); ?>
 		</div>
 		<div class="profile-editor">
-			<h5><?php _e('Tentang Toko', 'makery'); ?></h5>
+			<h5><?php _e('Tentang', 'makery'); ?></h5>
 			<?php ThemexInterface::renderEditor('about', ThemexShop::$data['profile']['about']); ?>
 		</div>
+		// ini merupakan kode untuk aturan toko
 		<div class="profile-editor">
-			<h5><?php _e('Aturan Toko', 'makery'); ?></h5>
+			<h5><?php _e('Aturan', 'makery'); ?></h5>
 			<?php ThemexInterface::renderEditor('policy', ThemexShop::$data['profile']['policy']); ?>
 		</div>
+		// ini merupakan kode untuk aturan toko
 		<?php if(ThemexShop::$data['status']=='draft') { ?>
 			<?php if(ThemexCore::checkOption('shop_approve')) { ?>
 			<a href="#" class="element-button element-submit primary"><?php _e('Simpan', 'makery'); ?></a>
 			<?php } else { ?>
-			<a href="#" class="element-button element-submit primary"><?php _e('Submit untuk di Review', 'makery'); ?></a>
+			<a href="#" class="element-button element-submit primary"><?php _e('Masukkan', 'makery'); ?></a>
 			<?php } ?>		
 		<?php } else { ?>
-		<a href="#" class="element-button element-submit primary"><?php _e('Save Changes', 'makery'); ?></a>
+		<a href="#" class="element-button element-submit primary"><?php _e('Simpan', 'makery'); ?></a>
 		<a href="<?php echo get_permalink(ThemexShop::$data['ID']); ?>" target="_blank" title="<?php _e('Lihat', 'makery'); ?>" class="element-button square secondary">
 			<span class="fa fa-eye"></span>
 		</a>
@@ -103,8 +105,8 @@ Template Name: Shop
 		<div id="remove_form">
 			<div class="site-popup small">
 				<form class="site-form" method="POST" action="">
-					<p><?php _e('Apakah kamu yakin ingin menghapus toko ini secara permanen?', 'makery'); ?></p>
-					<a href="#" class="element-button element-submit primary"><?php _e('Hapus Toko', 'makery'); ?></a>
+					<p><?php _e('Anda yakin ingin menghapus toko ini secara permanen?', 'makery'); ?></p>
+					<a href="#" class="element-button element-submit primary"><?php _e('Hapus Tokonya', 'makery'); ?></a>
 					<input type="hidden" name="shop_id" value="<?php echo ThemexShop::$data['ID']; ?>" />
 					<input type="hidden" name="shop_action" value="remove_shop" />
 				</form>
